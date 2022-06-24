b5c0de30636ea4023174c8b0a8d7c7462c4e7c40
DONTKILLME <ashishnegi0511@gmail.com>
2022-06-24 13:43:02 +0530

    removed token

--- config/constants.py
+++ config/constants.py
@@ -1,5 +1,5 @@
 import os
-BOT_TOKEN = os.environ['BOT_TOKEN']
+# BOT_TOKEN = os.environ['BOT_TOKEN']
 # DATABASE_URL = os.environ['HEROKU_POSTGRESQL_GREEN_URL']
 DATABASE_URL ="postgres://wnslxdcswucslb:abeb5b1c4d0b864b165854c98020ef304eed5cea1ab93f413ec8f44e937ac80f@ec2-52-73-184-24.compute-1.amazonaws.com:5432/d4k7uv0dpspomr"
 #BOT_TOKEN = "OTg5MDc3NzU4MzkzMDc3NzYy.GzbMFV.3meQ-NzyRQH-wSpIJ5WZB9yCpezHdQLW6XJj1o"
