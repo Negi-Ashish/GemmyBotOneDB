a24280b5b34b1d1ce9dbabb968974b5e907c17e5
DONTKILLME <ashishnegi0511@gmail.com>
2022-07-11 18:33:48 +0530

    test fetchall for none

--- botOneFun.py
+++ botOneFun.py
@@ -160,10 +160,9 @@ async def fd_earn(userID,data):
         cur.execute(query_read)
         con.commit()
         record = cur.fetchall()
-        print(record)
-        return "lol"
+        # [(datetime.datetime(2022, 7, 11, 12, 50, 26, 786131), 400, 656)]
         dt = datetime.now()
-        if record == None:
+        if record[0] == None:
             query = f"""UPDATE user_account SET "bank_balance"=("bank_balance"-'{data['amount']}'),"fd_start"='{dt}',"fixed_deposit"='{data['amount']}' Where ("user_id"='{userID}') """
             cur.execute(query)
             con.commit()
